c992265ab41718e3758a90efdd8aef2920162ba1
jaclealmena <jac.leal@duocuc.cl>
2024-05-29 10:48:43 -0400

    menu

---
+++ Act_3.1.2_Listas_Leal.py
@@ -0,0 +1,15 @@
+import os
+import msvcrt
+os.system("cls")
+
+while True:
+    print("""
+Menú
+
+1. Inicio sesión.
+2. Registrar usuario.
+3. Eliminar usuario.
+4. Salir.
+""")
+    opc = int(input("Opción: "))
+    
